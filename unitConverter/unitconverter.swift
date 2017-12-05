b67d9a52fcb404a289fcfa963171b0594b5c65eb
JINAA <jinagold.k@gmail.com>
2017-12-05 18:50:19 +0900

    Modify source 1. function name to verse 2. readline was made function 3. set parameter and return value in function 4. exception handling, if readline is empty

--- unitConverter/unitconverter.swift
+++ unitConverter/unitconverter.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 /*
-STEP 1. 길이 변환 및 출력
+ STEP 1. 길이 변환 및 출력
  
-- 다음의 센티미터(cm) 단위 값을 미터(m) 단위로 변환해 결과 화면에 출력한다.
-    예를 들어 "120cm"을 "1.2m"로 출력한다.
-- 미터(m) 단위 값을 센티미터(cm) 단위로 변환해 결과 화면에 출력한다.
-    "1.86m"를 "186cm"로 출력한다.
-*/
+ - 다음의 센티미터(cm) 단위 값을 미터(m) 단위로 변환해 결과 화면에 출력한다.
+ 예를 들어 "120cm"을 "1.2m"로 출력한다.
+ - 미터(m) 단위 값을 센티미터(cm) 단위로 변환해 결과 화면에 출력한다.
+ "1.86m"를 "186cm"로 출력한다.
+ */
 
 // cm -> m
 func cmToM(num:Double) -> Double{
@@ -20,14 +20,14 @@ func mToCm(num:Double) -> Double {
 }
 //----------------------------------------구분선---------------------------------------------------------------
 /*
-STEP 2. 길이 단위 변환 함수
+ STEP 2. 길이 단위 변환 함수
  
-- 센티미터 단위 값을 변수에 저장하고 변환하는 데 사용한다.
-- 센티미터 단위 값을 저장한 변수를 미터 단위 값으로 변환한 후 변수에 저장하고 저정한 변수 값을 출력한다.
-- 길이 단위를 바꿀 때 곱하거나 나누는 값은 바뀌지 않는 값이다. 따라서 상수 값으로 지정해서 프로그램을 구현한다.
-- 문자열로 값 뒤에 붙어있는 단위에 따라 길이를 변환해서 결과를 출력하는 함수를 만든다.
-    예를 들어 "183cm"처럼 숫자 다음에 cm가 붙어있으면 센티미터 값을 미터로 변환하고 출력한다. "3.14m"처럼 숫자 다음에 m가 붙어있으면 미터 값을
-    센티미터로 변환하고 출력한다.
+ - 센티미터 단위 값을 변수에 저장하고 변환하는 데 사용한다.
+ - 센티미터 단위 값을 저장한 변수를 미터 단위 값으로 변환한 후 변수에 저장하고 저정한 변수 값을 출력한다.
+ - 길이 단위를 바꿀 때 곱하거나 나누는 값은 바뀌지 않는 값이다. 따라서 상수 값으로 지정해서 프로그램을 구현한다.
+ - 문자열로 값 뒤에 붙어있는 단위에 따라 길이를 변환해서 결과를 출력하는 함수를 만든다.
+ 예를 들어 "183cm"처럼 숫자 다음에 cm가 붙어있으면 센티미터 값을 미터로 변환하고 출력한다. "3.14m"처럼 숫자 다음에 m가 붙어있으면 미터 값을
+ 센티미터로 변환하고 출력한다.
  */
 
 // cm <-> m
@@ -43,52 +43,64 @@ func unitConvert(num:Double,unit:String) -> Double {
 }
 //----------------------------------------구분선---------------------------------------------------------------
 /*
-STEP 3. 길이 값 입력 및 조건문
+ STEP 3. 길이 값 입력 및 조건문
  
-- 사용자가 길이 값을 입력하고 변수에 저장하도록 한다.
-- 길이 단위에 따라 센티미터를 미터로 바꾸는 함수와, 미터를 센티미터로 바꾸는 함수로 나눈다.
-- 사용자가 입력한 문자열에서 값 뒤에 붙어있는 단위에 따라서, 앞서 나눠놓은 길이 변환 함수를 호출하고 결과를 출력한다.
-    예를 들어 "183cm"처럼 숫자 다음에 cm가 붙어있으면 센티미터 값을 미터로 변환하고 출력한다.
-    "3.14m"처럼 숫자 다음에 m가 붙어있으면 미터 값을 센티미터로 변환하고 출력한다.
-*/
+ - 사용자가 길이 값을 입력하고 변수에 저장하도록 한다.
+ - 길이 단위에 따라 센티미터를 미터로 바꾸는 함수와, 미터를 센티미터로 바꾸는 함수로 나눈다.
+ - 사용자가 입력한 문자열에서 값 뒤에 붙어있는 단위에 따라서, 앞서 나눠놓은 길이 변환 함수를 호출하고 결과를 출력한다.
+ 예를 들어 "183cm"처럼 숫자 다음에 cm가 붙어있으면 센티미터 값을 미터로 변환하고 출력한다.
+ "3.14m"처럼 숫자 다음에 m가 붙어있으면 미터 값을 센티미터로 변환하고 출력한다.
+ */
 
-print("값을 입력해주세요.")
-let inputValue = readLine()
+print("값을 입력해주세요 -> 숫자+단위\n끝내려면 end를 입력해주세요")
 
-var cmOrM = ""
-var num = 0.0
-
-// inputValue를 숫자와 cm/m 분리
-if let inputValue = inputValue {
-    cmOrM = inputValue.trimmingCharacters(in: CharacterSet.letters.inverted)//입력값 문자만 가져옴
-    num = Double(inputValue.trimmingCharacters(in: CharacterSet.decimalDigits.inverted))!//입력값 숫자만 가져옴
+// 입력값 받기
+func getInput() -> String{
+    let input = readLine() ?? "end"
+    return input
 }
 
-//cm -> m
-func centiToM() {
+// inputValue에서 cm/m 분리
+func getUnitString(_ userInput: String) -> String {
+    var cmOrM = ""
+    cmOrM = userInput.trimmingCharacters(in: CharacterSet.letters.inverted) // 입력값 문자만 가져옴
+    return cmOrM
+}
+
+// inputValue에서 숫자만 분리
+func getUnitNumber(_ userInput: String) -> Double {
+    var num = 0.0
+    num = Double(userInput.trimmingCharacters(in: CharacterSet.decimalDigits.inverted))! // 입력값 숫자만 가져옴
+    return num
+}
+
+// cm -> m
+func convertCentiToM(_ userInput: String) {
     var result = 0.0
     let meter = "m"
-    result = num / Double(100)
+    result = getUnitNumber(userInput) / Double(100)
     print("\(result)\(meter)")
 }
 
-//m -> cm
-func meterToCm() {
+// m -> cm
+func convertMeterToCm(_ userInput: String) {
     var result = 0.0
-    let meter = "cm"
-    result = num * Double(100)
-    print("\(result)\(meter)")
+    let centi = "cm"
+    result = getUnitNumber(userInput) * Double(100)
+    print("\(result)\(centi)")
 }
 
-//cm <-> m
-func conversion() {
-    if cmOrM == "cm" {
-        centiToM()
-    } else {
-        meterToCm()
+// cm <-> m
+while true {
+    let userInput = getInput()
+    if userInput == "end" {
+        break
+    }
+    if getUnitString(userInput) == "cm" {
+        convertCentiToM(userInput)
+    } else if getUnitString(userInput) == "m" {
+        convertMeterToCm(userInput)
     }
 }
-
-conversion()
 
 
