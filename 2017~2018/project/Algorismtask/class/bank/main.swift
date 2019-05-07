f6ed1bf868a51914d7726e153dfe07ae9f9723b5
김지나 <jinagold.k@gmail.com>
2019-05-07 16:46:59 +0900

    불필요한 파일 삭제

--- 2017~2018/project/Algorismtask/class/bank/main.swift
+++
@@ -1,7 +0,0 @@
-let account1: Account = Account(bankName: "우리", money: 3000)
-let account2: Account = Account(bankName: "카카오", money: 5000)
-let customer1 : Custom = Custom(name: "라이언", address: "강남")
-
-customer1.acc = [account1, account2]
-print(customer1.sumMoney())
-
