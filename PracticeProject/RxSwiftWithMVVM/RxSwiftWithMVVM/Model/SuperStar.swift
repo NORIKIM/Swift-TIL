cd6af15ae221ac7f557d89057f9c111d72471198
NORIKIM <jinagold.k@gmail.com>
2023-07-16 17:43:16 +0900

    model 생성

---
+++ PracticeProject/RxSwiftWithMVVM/RxSwiftWithMVVM/Model/SuperStar.swift
@@ -0,0 +1,20 @@
+//
+//  SuperStar.swift
+//  RxSwiftWithMVVM
+//
+//  Created by 김지나 on 2023/07/16.
+//
+
+import Foundation
+
+final class SuperStar {
+    let name: String
+    let club: String
+    let avator: String
+    
+    init(name: String, club: String, avator: String) {
+        self.name = name
+        self.club = club
+        self.avator = avator
+    }
+}
