1b186726d7ddf3f7e77f659a19db56ecd1d57365
NORIKIM <jinagold.k@gmail.com>
2023-07-16 17:38:03 +0900

    RxSwift with MVVM 연습 프로젝트 생성

---
+++ PracticeProject/RxSwiftWithMVVM/RxSwiftWithMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftWithMVVM
+//
+//  Created by 김지나 on 2023/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
